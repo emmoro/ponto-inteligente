5e55e53ae6328d908a8e66d63c2648e4348039d0
emmoro <eltonmoroju@gmail.com>
2020-10-26 18:48:47 -0300

    Primeiro commite projeto ponto inteligente

---
+++ src/test/java/com/br/elton/pontointeligente/api/PontoInteligenteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.elton.pontointeligente.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontoInteligenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
